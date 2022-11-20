1b4f0b372961916f01e535f56c60ab1f969b8b1d
rmarrcode <ryanmarr@Ryans-MacBook-Air.local>
2022-11-20 12:29:24 -0800

    clean

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,10 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; // TO get bootstr
 // import reportWebVitals from './misc/reportWebVitals';
 
 import { BrowserRouter, Routes, Route, } from "react-router-dom"
-import { AuthProvider } from './new_components/auth/useAuth';
+import { AuthProvider } from './components/auth/useAuth';
 
-// RYAN TODO: EVENTUALLY ADD THIS BACK
-// import KYC from './new_components/kyc/kyc';
-
-// Shiny new Components
-import MatadorLanding from './new_components/landing/landing';
-import MatadorPage from './new_components/base/page';
-import MatadorSignin from "./new_components/auth/signin";
-import MatadorSignUp from "./new_components/auth/signup";
+import MatadorLanding from './components/landing/landing';
+import MatadorSignin from "./components/auth/signin";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -23,7 +17,6 @@ root.render(
     <AuthProvider>
         <Routes>
           <Route path="/" element={<MatadorLanding/>} />
-          <Route path="/signup" element={<MatadorSignUp />} />
           <Route path="/signin" element={<MatadorSignin />} />
         </Routes> 
     </AuthProvider>
